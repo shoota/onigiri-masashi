4771cae0102c05fdf8244a92bffa97f7d914eab2
Shuta Kumano <shoota.dev@gmail.com>
2024-02-10 20:17:32 +0900

    fix: env value

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import fs from "fs"
 // https://vitejs.dev/config/
 
 const server =
-  process.env.DEPLOY_PLATFORM === "vite"
+  process.env.DEPLOY_PLATFORM === "vercel"
     ? undefined
     : {
         https: {
